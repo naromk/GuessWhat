f7f19ab86b36a9f8c99320411bd29e3d92c40349
narom kuoch <narom@naroms-MacBook-Pro.local>
2018-05-09 22:24:18 -0500

    Adding some java doc.

--- app/src/main/java/com/nk/guesswhat/StartActivity.java
+++ app/src/main/java/com/nk/guesswhat/StartActivity.java
@@ -3,6 +3,10 @@ package com.nk.guesswhat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Initial activity that will be launched when the application
+ * gets loaded.  
+ */
 public class StartActivity extends AppCompatActivity {
 
     @Override
